6b1aa1a8e9efd9a1ef633dd9b476d7dcfd5e5e64
hseungho <hsh47607@gmail.com>
2024-02-24 18:49:03 +0900

    first commit
    Signed-off-by: hseungho <hsh47607@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# fc-springbatch-settlement
+# 스프링 배치 - 정산 시스템 강의
+##### by. fastcampus - [origin source code](https://github.com/VeritasLM/fastcampus-settlement-kotlin)
---
+++ src/main/kotlin/com/hseungho/study/fastcampus/batch/settlement/SettlementBatchApplication.kt
@@ -0,0 +1,11 @@
+package com.hseungho.study.fastcampus.batch.settlement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SettlementBatchApplication
+
+fun main(args: Array<String>) {
+    runApplication<SettlementBatchApplication>(*args)
+}
